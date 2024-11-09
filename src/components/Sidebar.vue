a7f40c8d4d0c6da65c37a30e13c1627e0dd27201
Jatinder-Sohal <jsohal220@gmail.com>
2024-11-09 13:16:18 +0000

    Made changes to css of sidebar elements, renamed them to be more accurate, changed hover effects and added placeholders for icons.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -6,13 +6,27 @@
 
 
 <template>
-     <aside class="sidebar">
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
+    <aside class="sidebar">
+        <div class="profile">
+            <div class="profile-pic">
+                <span>JS</span>
+            </div>
+            <a class="name">Jatinder Sohal</a>
+            <p class="tagline">Computer Science Graduate</p>
+        </div>
+        <nav class="nav-links">
+            <a href="#">Current Project</a>
+            <a href="#">All Projects</a>
+            <a href="#">Experiance & Education</a>
+            <a href="#">Skills</a>
+        </nav>
+        <div class="icons">
+            <a href="#">Github</a>
+            <a href="#">Resume</a>
+            <a href="#">Email</a>
+
+        </div>
     </aside>
-    
-   
 </template>
 
 <style scoped>
@@ -26,13 +40,44 @@
     flex-direction: column;
     color: white;
     text-align: center;
-    align-content: center;
+    align-items: center;
     justify-content: center;
     transition: width 0.3s ease; 
     width: 23vw; 
-    max-width: 435px; 
+    max-width: 455px; 
     min-width: 150px;
 }
+.profile {
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    margin-bottom: 5%;
+}
+.profile-pic{
+    background-color: #333;
+    width: 140px;
+    height: 140px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 6%;
+}
+.name{
+    font-size: 2.1rem;
+    font-weight: bold;
 
-
+}
+.tagline{
+    font-size: 1.3rem;
+}
+.nav-links{
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 5%;
+    font-size: 1.1rem;
+}
+.icons{
+    margin-bottom: 15%;
+}
 </style>
